09fd0afdabf54dbbd77c87d881d649b92f5d7595
zack-zheng <121322680@qq.com>
2017-08-20 21:21:22 +0800

    如果文件夹不存在，递归生成文件夹

--- zheng-common/src/main/java/com/zheng/common/util/VelocityUtil.java
+++ zheng-common/src/main/java/com/zheng/common/util/VelocityUtil.java
@@ -16,53 +16,69 @@ import java.util.Properties;
  */
 public class VelocityUtil {
 
-	/**
-	 * 根据模板生成文件
-	 * @param inputVmFilePath 模板路径
-	 * @param outputFilePath 输出文件路径
-	 * @param context
-	 * @throws Exception
-	 */
-	public static void generate(String inputVmFilePath, String outputFilePath, VelocityContext context) throws Exception {
-		try {
-			Properties properties = new Properties();
-			properties.setProperty(VelocityEngine.FILE_RESOURCE_LOADER_PATH, getPath(inputVmFilePath));
-			Velocity.init(properties);
-			//VelocityEngine engine = new VelocityEngine();
-			Template template = Velocity.getTemplate(getFile(inputVmFilePath), "utf-8");
-			File outputFile = new File(outputFilePath);
-			FileWriterWithEncoding writer = new FileWriterWithEncoding(outputFile, "utf-8");
-			template.merge(context, writer);
-			writer.close();
-		} catch (Exception ex) {
-			throw ex;
-		}
-	}
+    /**
+     * 根据模板生成文件
+     *
+     * @param inputVmFilePath 模板路径
+     * @param outputFilePath  输出文件路径
+     * @param context
+     * @throws Exception
+     */
+    public static void generate(String inputVmFilePath, String outputFilePath, VelocityContext context) throws Exception {
+        try {
+            Properties properties = new Properties();
+            properties.setProperty(VelocityEngine.FILE_RESOURCE_LOADER_PATH, getPath(inputVmFilePath));
+            Velocity.init(properties);
+            //VelocityEngine engine = new VelocityEngine();
+            Template template = Velocity.getTemplate(getFile(inputVmFilePath), "utf-8");
+            File outputFile = new File(outputFilePath);
+            if (!outputFile.getParentFile().exists()) {
+                mkDir(outputFile.getParentFile());
+            }
+            FileWriterWithEncoding writer = new FileWriterWithEncoding(outputFile, "utf-8");
+            template.merge(context, writer);
+            writer.close();
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
 
-	/**
-	 * 根据文件绝对路径获取目录
-	 * @param filePath
-	 * @return
-	 */
-	public static String getPath(String filePath) {
-		String path = "";
-		if (StringUtils.isNotBlank(filePath)) {
-			path = filePath.substring(0, filePath.lastIndexOf("/") + 1);
-		}
-		return path;
-	}
+    // 递归创建文件夹
+    public static void mkDir(File file) {
+        if (file.getParentFile().exists()) {
+            file.mkdir();
+        } else {
+            mkDir(file.getParentFile());
+            file.mkdir();
+        }
+    }
 
-	/**
-	 * 根据文件绝对路径获取文件
-	 * @param filePath
-	 * @return
-	 */
-	public static String getFile(String filePath) {
-		String file = "";
-		if (StringUtils.isNotBlank(filePath)) {
-			file = filePath.substring(filePath.lastIndexOf("/") + 1);
-		}
-		return file;
-	}
+    /**
+     * 根据文件绝对路径获取目录
+     *
+     * @param filePath
+     * @return
+     */
+    public static String getPath(String filePath) {
+        String path = "";
+        if (StringUtils.isNotBlank(filePath)) {
+            path = filePath.substring(0, filePath.lastIndexOf("/") + 1);
+        }
+        return path;
+    }
+
+    /**
+     * 根据文件绝对路径获取文件
+     *
+     * @param filePath
+     * @return
+     */
+    public static String getFile(String filePath) {
+        String file = "";
+        if (StringUtils.isNotBlank(filePath)) {
+            file = filePath.substring(filePath.lastIndexOf("/") + 1);
+        }
+        return file;
+    }
 
 }
